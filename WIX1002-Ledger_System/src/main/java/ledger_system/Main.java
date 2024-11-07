fdc866a221a9cc7f7f77bf3cd8026ab5de92ad51
Cyhandleit <hermanchia118@gmail.com>
2024-11-08 00:05:09 +0800

    Registration and Login Interface ONLY

---
+++ WIX1002-Ledger_System/src/main/java/ledger_system/Main.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ledger_system;
+import java.util.*;
+/**
+ *
+ * @author User
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        
+        Scanner s = new Scanner(System.in);
+        
+        System.out.println("== Ledger System (name can created yourself) ==");
+        System.out.println("Login or Register: ");
+        System.out.println("1.Login");
+        System.out.println("2.Register");
+        
+        System.out.println("== Please fill in the form ==");
+        System.out.print("Name: ");
+        System.out.print("Email: ");
+        System.out.print("Password: ");
+        
+        System.out.println("== Please enter your email and password ==");
+        
+    }
+}
