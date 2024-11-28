d106770dd4814842fd611201e6b1546d512204c4
Fankey <arfan.izanagi456@gmail.com>
2024-11-28 14:22:12 +0800

    new branch

--- WIX1002-Ledger_System/src/main/java/ledger_system/Main.java
+++ WIX1002-Ledger_System/src/main/java/ledger_system/Main.java
@@ -2,29 +2,86 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
 
-package ledger_system;
-import java.util.*;
-/**
- *
- * @author User
- */
-public class Main {
-
-    public static void main(String[] args) {
-        
-        Scanner s = new Scanner(System.in);
-        
-        System.out.println("== Ledger System (name can created yourself) ==");
-        System.out.println("Login or Register: ");
-        System.out.println("1.Login");
-        System.out.println("2.Register");
-        
-        System.out.println("== Please fill in the form ==");
-        System.out.print("Name: ");
-        System.out.print("Email: ");
-        System.out.print("Password: ");
-        
-        System.out.println("== Please enter your email and password ==");
-        
-    }
-}
+ package ledger_system;
+ import java.util.Scanner;
+ /**
+  *
+  * @author User
+  */
+ 
+ public class Main {
+     static Scanner s = new Scanner(System.in);
+ 
+     public static void main(String[] args) {
+ 
+         int choice;
+         while (true) {
+             System.out.println("\nLogin or Register :");
+             System.out.println("1. Login");
+             System.out.println("2. Register");
+             System.out.println();
+ 
+             choice = s.nextInt();
+ 
+             switch (choice) {
+                 case 1:
+                     loginPage();
+ 
+                     break;
+ 
+                 case 2:
+                     registerPage();
+                     break;
+ 
+                 default:
+                     System.out.println("Enter a valid number !");
+                     break;
+             }
+         }
+     }
+ 
+     // login page (boolean to see if login successful or not)
+     public static boolean loginPage() {
+ 
+         String input_email;
+         String input_pass;
+ 
+         System.out.println("\n== Please enter your email and password == ");
+         System.out.print("email : ");
+         input_email = s.next();
+         
+         System.out.print("password : ");
+         input_pass = s.next();
+ 
+         if (!Data.email.equals(input_email) || !Data.pass.equals(input_pass)){
+             System.out.println("Username or password is incorrect!");
+             return false;
+         } else {
+             System.out.println("\nLogin Successful!");
+             System.out.println("== Welcome, " + Data.name + " ==");
+             return true;
+         }
+ 
+     }
+ 
+     // register page
+     public static void registerPage() {
+ 
+         System.out.println("\n== Please fill in the form ==");
+         System.out.print("username : ");
+         Data.name = s.next();
+ 
+         System.out.print("email : ");
+         Data.email = s.next();
+ 
+         System.out.print("password : ");
+         Data.pass = s.next();
+ 
+ 
+         System.out.println("\nRegister Successful!");
+ 
+     }
+ 
+ }
+ 
+ 
